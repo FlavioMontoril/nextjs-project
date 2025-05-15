f586cfedecfc11bba6bcfda03c83795fddcf852b
Flavio Montoril <flavio.montorilll@gmail.com>
2025-05-15 15:49:40 -0300

    Primeiro commit

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+    return (
+        <div className="bg-green-300 w-full h-screen">
+            <h1>Page Sign In</h1>
+        </div>
+    )
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="bg-amber-300 w-full h-screen">
+            <h1>About</h1>
+        </div>
+    )
+}
